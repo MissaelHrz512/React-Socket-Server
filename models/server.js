3d3236f435da31801aa76326c73526b68567dfe2
MissaelHrz512 <ubunto67@gmail.com>
2026-04-09 10:49:08 -0600

    CORS ADDED

--- models/server.js
+++ models/server.js
@@ -3,6 +3,7 @@ const http = require("http");
 const socketio = require("socket.io");
 const path = require("path");
 const Sockets = require("./sockets");
+const cors = require("cors")
 class Server {
   constructor() {
     this.app = express();
@@ -16,6 +17,7 @@ class Server {
   }
   middlewares() {
     this.app.use(express.static(path.resolve(__dirname, "../public")));
+    this.app.use(cors())
   }
 
   socketConfig() {
